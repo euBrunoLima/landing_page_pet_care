d026ec287a3f004763f25ee91d333d6a32a4f6d0
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2025-02-04 17:37:20 -0300

    primeiro commit header

---
+++ src/javascript/script.js
@@ -0,0 +1,10 @@
+const hamburguer_menu = document.getElementById('hamburguer_menu')
+const mobile_menu = document.getElementById('mobile_menu')
+
+
+function showMenu(){
+    hamburguer_menu.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+
+
+}
