2eae0c1d7f6ad6f9041cfb6264849083b7d93f0f
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2025-02-05 18:15:33 -0300

    melhorias no header

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,10 +1,17 @@
 const hamburguer_menu = document.getElementById('hamburguer_menu')
 const mobile_menu = document.getElementById('mobile_menu')
+const btn_mobile_header = document.getElementById('nav_list_mobile')
+const login_mobile = document.querySelector('.login-mobile')
 
+console.log(login_mobile);
 
 function showMenu(){
     hamburguer_menu.classList.toggle('active');
     mobile_menu.classList.toggle('active');
+    btn_mobile_header.classList.toggle('active');
+    login_mobile.classList.toggle('active');
+
+    
 
 
 }
